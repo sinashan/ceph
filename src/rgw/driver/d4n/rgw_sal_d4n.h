97723efc38085bbd36c191921047d69a0ac8979b
sinashan <sinatak1373@live.com>
2024-06-19 13:07:58 -0400

    testing bucket listing

--- src/rgw/driver/d4n/rgw_sal_d4n.h
+++ src/rgw/driver/d4n/rgw_sal_d4n.h
@@ -79,7 +79,7 @@ class D4NFilterDriver : public FilterDriver {
     virtual void register_admin_apis(RGWRESTMgr* mgr) override;
     virtual std::unique_ptr<User> get_user(const rgw_user& u) override;
     virtual const std::string get_name() const override;
-    virtual int load_bucket(const DoutPrefixProvider* dpp, optional_yield y) override;
+
     virtual std::unique_ptr<Object> get_object(const rgw_obj_key& k) override;
 
     virtual std::unique_ptr<Writer> get_atomic_writer(const DoutPrefixProvider *dpp,
