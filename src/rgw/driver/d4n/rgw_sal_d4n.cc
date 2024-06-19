2f6f4db1f07517ddba4fc8dc1150d7e835d4fd6b
sinashan <sinatak1373@live.com>
2024-06-19 19:08:14 -0400

    testing bucket listing

--- src/rgw/driver/d4n/rgw_sal_d4n.cc
+++ src/rgw/driver/d4n/rgw_sal_d4n.cc
@@ -179,7 +179,7 @@ int D4NFilterBucket::list(const DoutPrefixProvider* dpp, ListParams& params, int
   ldpp_dout(dpp, 20) << "D4NFilterBucket::" << __func__ << " Bucket Name: " << next->get_name() << dendl;
 
   //return next->list(dpp, params, max, results, y);
-  return "salam";
+  return 4;
 }
 
 int D4NFilterObject::copy_object(User* user,
