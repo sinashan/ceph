9533656b22f15b527a08e29527edfe4258818753
sinashan <sinatak1373@live.com>
2024-06-24 12:05:50 -0400

    testing reading from cache

--- src/rgw/driver/d4n/rgw_sal_d4n.cc
+++ src/rgw/driver/d4n/rgw_sal_d4n.cc
@@ -177,7 +177,7 @@ int D4NFilterBucket::list(const DoutPrefixProvider* dpp, ListParams& params, int
 		       ListResults& results, optional_yield y)
 {
   ldpp_dout(dpp, 20) << "D4NFilterBucket::" << __func__ << " Bucket Name: " << next->get_name() << dendl;
-  auto completed = source->driver->get_cache_driver();
+  auto completed = filter->get_cache_driver();
   
   int ret = next->list(dpp, params, max, results, y);
 
