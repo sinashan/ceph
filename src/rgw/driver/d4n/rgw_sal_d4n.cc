d6bcd7ac940c01f4e8cfc12caef9078768cd9a43
sinashan <sinatak1373@live.com>
2024-06-25 21:12:26 -0400

    testing bucket listing

--- src/rgw/driver/d4n/rgw_sal_d4n.cc
+++ src/rgw/driver/d4n/rgw_sal_d4n.cc
@@ -220,7 +220,7 @@ int D4NFilterBucket::list(const DoutPrefixProvider* dpp, ListParams& params, int
               // int ret = flush(dpp, std::move(completed), y);
               ldpp_dout(dpp, 20) << "D4NFilterBucket::" << __func__ << " Ret: " << ret << dendl;
               bufferlist bl;
-              auto r = cb->handle_data(bl, start_offset, read_length-start_offset);
+              auto r = client_cb->handle_data(bl, start_offset, read_length-start_offset);
               // ldpp_dout(dpp, 20) << "D4NFilterBucket::" << __func__ << " Line: " << __LINE__ << dendl;
 
               // Get the file information
