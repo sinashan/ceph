aa21165b207f5100c108c65fbab0329b7120326d
sinashan <sinatak1373@live.com>
2024-06-27 14:36:56 -0400

    testing reading from cache

--- src/rgw/driver/d4n/rgw_sal_d4n.cc
+++ src/rgw/driver/d4n/rgw_sal_d4n.cc
@@ -183,27 +183,32 @@ int D4NFilterBucket::list(const DoutPrefixProvider* dpp, ListParams& params, int
     std::string bucket_name = next->get_name();
     std::string cache_location = g_conf()->rgw_d4n_l1_datacache_persistent_path;
 
+    std::unordered_map<std::string, uint64_t> object_sizes;
+
     DIR* dir;
     struct dirent* ent;
     if ((dir = opendir(cache_location.c_str())) != NULL) {
         while ((ent = readdir(dir)) != NULL) {
             std::string file_name = ent->d_name;
+            
+            std::vector<std::string> parts;
+            std::string part;
+            std::stringstream ss(file_name);
+            while (std::getline(ss, part, '_')) {
+                parts.push_back(part);
+            }
+  
+            std::string bucket_name = parts[0];
+            std::string object_version = parts[1];
+            std::string object_name = parts[2];
+            int start_offset = atoi(parts[3].c_str());
+            int read_length = atoi(parts[4].c_str());
+
+            object_sizes[object_name] += read_length;
 
             if (file_name.rfind(bucket_name + "_", 0) == 0) {
               std::string full_path = cache_location + "/" + file_name;
 
-              std::vector<std::string> parts;
-              std::stringstream ss(file_name);
-              std::string part;
-              while (std::getline(ss, part, '_')) {
-                  parts.push_back(part);
-              }
-    
-              std::string bucket_name = parts[0];
-              std::string object_version = parts[1];
-              std::string object_name = parts[2];
-              int start_offset = atoi(parts[3].c_str());
-              int read_length = atoi(parts[4].c_str());
 
               rgw_bucket_dir_entry new_entry;
 
@@ -229,7 +234,13 @@ int D4NFilterBucket::list(const DoutPrefixProvider* dpp, ListParams& params, int
         ldpp_dout(dpp, 0) << "Could not open directory " << cache_location << dendl;
     }
   }
-
+  for (auto& pair : object_sizes) {
+    std::string object_name = pair.first;
+    uint64_t object_size = pair.second;
+    // Now you can use object_name and object_size
+    ldpp_dout(dpp, 20) << "Object Name: " << object_name \
+                        << "Object Size: " << object_size << dendl;
+  }
   return ret;
 }
 
