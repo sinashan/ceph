26f9722b1466475e7a36e18dcb78c1785f934c25
sinashan <sinatak1373@live.com>
2025-01-22 11:28:07 -0500

    bucket listing with directory
    Signed-off-by: sinashan <sinatak1373@live.com>

--- src/rgw/driver/d4n/d4n_directory.h
+++ src/rgw/driver/d4n/d4n_directory.h
@@ -83,6 +83,7 @@ class ObjectDirectory: public Directory {
     */
 
     int exist_key(CacheObj* object, optional_yield y);
+    int get_bucket_keys(const DoutPrefixProvider* dpp, std::string bucket_name, std::vector <CacheObj*>* objects, optional_yield y);
     //void shutdown();
     int set(CacheObj* object, optional_yield y);
     int get(CacheObj* object, optional_yield y);
